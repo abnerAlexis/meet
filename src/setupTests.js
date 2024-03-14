76ffa441c3ecd1a4737cfc5997dabc8d45a61e98
Alexis Abner <alexisabner@alexiss-air.lan>
2024-03-13 22:31:50 -0600

    Update setupTests.js to clear out the warning messages, update CitySearc.js and CitySearch.test.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,15 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+//Here, add portions of the warning messages you want to intentionally prevent from appearing
+
+const MESSAGE_TO_IGNORE = [
+    "When testing, code that causes React state updates should be wrapped in to act(...):",
+    "Error:",
+    "The above error Ocurred"
+];
+
+const originalError = console.error.bind(console.error);
+
+console.error = (...args) => {
+    const ignoreMessage = MESSAGE_TO_IGNORE.find(message => args.toString().includes(message));
+    if(!ignoreMessage) originalError(...args);
+}
