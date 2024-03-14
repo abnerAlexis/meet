183b0b1939491a16f30951474ad32b7943663c2f
Alexis Abner <alexisabner@alexiss-air.lan>
2024-03-14 17:47:16 -0600

    Add mock-data.js into src folder

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -78,7 +78,7 @@ module.exports.getCalendarEvents = async (event) => {
   return new Promise((resolve, reject) => {
     calendar.events.list(
       {
-        calendarId: CALENDAR_ID,
+        calendarId: CALENDAR_ID, //in 4.4 it is calendar_id
         auth: oAuth2Client,
         timeMin: new Date().toISOString(),
         singleEvents: true,
