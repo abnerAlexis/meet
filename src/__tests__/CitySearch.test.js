d9a2500f5f8bd3a728ac19a8aad1f604596f820d
Alexis Abner <alexisabner@alexiss-air.lan>
2024-03-15 19:57:47 -0600

    Add test to compare user event click and suggestions

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -54,5 +54,20 @@ describe('<CitySearch /> component', () => {
         }
     });
 
+    test('renders the suggestion text in the textbox up on clicking on the suggestion', async () => {
+        const user = userEvent.setup();
+        const allEvents = await getEvents();
+        const allLocations = extractLocations(allEvents);
+        CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
 
+        const cityTextBox = CitySearchComponent.queryByRole('textbox');
+        await user.type(cityTextBox, "Berlin");
+
+        // the suggestion's textContent look like this: "Berlin, Germany"
+        const BerlinGermanySuggestion = CitySearchComponent.queryAllByRole('listitem')[0];
+
+        await user.click(BerlinGermanySuggestion);
+
+        expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    });
 });
