6af050d9bb230ab5600897864d4108f26821663f
Alexis Abner <alexisabner@alexiss-air.lan>
2024-03-23 18:53:11 -0600

    Add functions getToken, checkToken, removeQuery and update getEvents

--- src/api.js
+++ src/api.js
@@ -16,13 +16,55 @@ export const extractLocations = (events) => {
     return locations;
 };
 
+const getToken = async (code) => {
+    try {
+        const encodeCode = encodeURIComponent(code);
+        const response = await fetch('https://vdeytmzi3m.execute-api.eu-central-1.amazonaws.com/dev/api/token/' + encodeCode);
+        const { access_token } = await response.json();
+        access_token && localStorage.setItem("access_token", access_token);
+        return access_token;
+
+    } catch (error) {
+        error.json();
+    }
+}
+
+const checkToken = async (accessToken) => {
+    const response = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`);
+    return response.json;
+}
+
+const removeQuery = () => {
+    let newurl;
+    if (window.history.pushState && window.location.pathname) {
+        newurl = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        window.history.pushState("", "", newurl);
+    } else {
+        newurl = window.location.protocol + "//" + window.location.host;
+        window.history.pushState("", "", newurl);
+    }
+}
+
 /**
  *
  * This function will fetch the list of all events
  */
 export const getEvents = async () => {
-    // console.log(mockData)
-    return mockData;
+    if (window.location.href.startsWith('http://localhost')) {
+        return mockData;
+    }
+
+    const token = await getAccessToken();
+
+    if (token) {
+        removeQuery();
+        const url = "ttps://vdeytmzi3m.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/" + token;
+        const response = await fetch(url);
+        const result = await response.json();
+        if (result) {
+            return result.events;
+        } else return null;
+    }
 }
 
 export const getAccessToken = async () => {
@@ -34,12 +76,12 @@ export const getAccessToken = async () => {
         const searchParams = new URLSearchParams(window.location.search);
         const code = await searchParams.get("code");
         if (!code) {
-            const response = await fetch("YOUR_SERVERLESS_GET_AUTH_URL_ENDPOINT");
+            const response = await fetch("https://vdeytmzi3m.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url");
             const result = await response.json();
             const { authUrl } = result;
             return(window.location.href = authUrl);
         }
-        return code && getAccessToken(code);
+        return code && getToken(code);
     }
     return accessToken;
 };
