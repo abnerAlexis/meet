44bc8e24c9515f137b8fccf1255adcdb668c6234
Alexis Abner <alexisabner@alexiss-air.lan>
2024-03-25 18:49:04 -0600

    fetcData is note used warning?

--- src/App.js
+++ src/App.js
@@ -13,16 +13,17 @@ const App = () => {
   const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
 
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === "See all cities" 
+      ? allEvents 
+      : allEvents.filter(event => event.location === currentCity)
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const allEvents = await getEvents();
-      const filteredEvents = currentCity === "See all cities" 
-        ? allEvents 
-        : allEvents.filter(event => event.location === currentCity)
-      setEvents(filteredEvents.slice(0, currentNOE));
-      setAllLocations(extractLocations(allEvents));
-    };
+    fetchData();
   }, [currentCity, currentNOE]);
 
   return (
