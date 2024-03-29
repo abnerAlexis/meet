f85544c1c50571e2d309ae03fb8e799664031485
Alexis Abner <alexisabner@alexiss-air.lan>
2024-03-28 22:18:29 -0600

    Add afterEach() to EndToEnd.test.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,15 +1,41 @@
 import puppeteer from 'puppeteer';
 
-describe('show/hide an event details', async () => {
-    const browser = await puppeteer.launch();
+describe('show/hide an event details', () => {
+    let browser;
+    let page;
 
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+        await page.waitForSelector('.event');
+    });
 
-    await page.waitForSelector('.event');
+    // Give time to any async operation to complete after each test
+  afterEach(async () => {
+    jest.setTimeout(20000);
+  });
 
-    const eventDetails = await page.$('eventdetails');
+    afterAll(async () => {
+        await browser.close();
+    });
 
-    expect(eventDetails).toBeNull();
-    browser.close();
+    test('Event element is collapsed by default', async () => {
+        const eventDetails = await page.$('eventdetails');
+        expect(eventDetails).toBeNull();
+    })
 });
+
+// describe('show/hide an event details', async () => {
+//     const browser = await puppeteer.launch();
+
+//     const page = await browser.newPage();
+//     await page.goto('http://localhost:3000/');
+
+//     await page.waitForSelector('.event');
+
+//     const eventDetails = await page.$('eventdetails');
+
+//     expect(eventDetails).toBeNull();
+//     browser.close();
+// });
