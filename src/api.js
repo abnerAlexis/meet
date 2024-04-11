6ba3696fe6b9847eee22e4dd530ddbc587768f7a
Alexis Abner <alexisabner@alexiss-air.lan>
2024-04-10 18:32:05 -0600

    Install NProgress and implement it into api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import mockData from "./mock-data";
+import NProgress from "nprogress";
 
 /**
  *
@@ -33,6 +34,8 @@ export const getEvents = async () => {
         const response = await fetch(url);
         const result = await response.json();
         if (result) {
+            NProgress.done();
+            localStorage.setItem("lastEvents", JSON.stringify(result.events));
             return result.events;
         } else return null;
     }
