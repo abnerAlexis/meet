55e7fd02063dcb43d034ea1bf99f98425f400588
Alexis Abner <alexisabner@alexiss-air.lan>
2024-04-13 22:07:28 -0600

    Create EventGenresChart containing a pie chart of genres

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { getEvents, extractLocations } from './api';
 import { InfoAlert, WarningAlert, ErrorAlert } from './components/Alert';
 import './App.css';
 import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 
 
 const App = () => {
@@ -72,10 +73,13 @@ const App = () => {
         setCrntNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
-      <CityEventsChart 
-        allLocations={ allLocations }
-        events={ events }
-      />
+      <div className='charts-container'>
+        <EventGenresChart events={events} />
+        <CityEventsChart 
+          allLocations={ allLocations }
+          events={ events }
+        />
+      </div>
       <EventList events={events} />
     </div>
   );
