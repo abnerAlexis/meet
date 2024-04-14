ecafa76d976da35534455d02f8b1a9606c0a74eb
Alexis Abner <alexisabner@alexiss-air.lan>
2024-04-13 22:30:15 -0600

    Create chart colors array

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -9,7 +9,8 @@ import {
 
 const EventGenresChart = ({ events }) => {
     const [data, setData] = useState([]);
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular']; 
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'Angular'];
+    const COLORS = ['#B74AFA', '#F67A53', '#53F6E2', '#16F516'];
 
     const getData = () => {
         const data = genres.map(genre => {
@@ -32,35 +33,39 @@ const EventGenresChart = ({ events }) => {
         const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
         const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
         return percent ? (
-          <text
-            x={x}
-            y={y}
-            fill="#8884d8"
-            textAnchor={x > cx ? 'start' : 'end'}
-            dominantBaseline="central"
-          >
-            {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
-          </text>
+            <text
+                x={x}
+                y={y}
+                fill="#8884d8"
+                textAnchor={x > cx ? 'start' : 'end'}
+                dominantBaseline="central"
+            >
+                {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+            </text>
         ) : null;
-      };
+    };
 
-return (
-    <ResponsiveContainer 
-        width='99%' 
-        height={400} 
-    >
-        <PieChart>
-            <Pie 
-                data={data}
-                dataKey='value'
-                fill='#8884d8'
-                labelLine={false}
-                label={renderCustomizedLabel}
-                outerRadius={130}
-            />
-        </PieChart>
-    </ResponsiveContainer>
-);
+    return (
+        <ResponsiveContainer
+            width='99%'
+            height={400}
+        >
+            <PieChart>
+                <Pie
+                    data={data}
+                    dataKey='value'
+                    fill='#8884d8'
+                    labelLine={false}
+                    label={renderCustomizedLabel}
+                    outerRadius={130}
+                >
+                    {data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                    ))}
+                </Pie>
+            </PieChart>
+        </ResponsiveContainer>
+    );
 
 }
 
